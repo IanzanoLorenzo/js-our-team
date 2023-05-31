99558614fa0e5ec28bf52c83c096d43ffeb62733
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-05-31 16:12:39 +0200

    inserito ciclo che mostra i dati in console

--- script/script.js
+++ script/script.js
@@ -3,33 +3,41 @@
 //array contenente i dati del team
 let ourTeam = [
     {
-        'name':'Wayne Barnett',
-        'role':'Founder & CEO',
-        'image':'wayne-barnett-founder-ceo.jpg'
+        'nome':'Wayne Barnett',
+        'ruolo':'Founder & CEO',
+        'foto':'wayne-barnett-founder-ceo.jpg'
     },
     {
-        'name':'Angela Caroll',
-        'role':'Chief Editor',
-        'image':'angela-caroll-chief-editor.jpg'
+        'nome':'Angela Caroll',
+        'ruolo':'Chief Editor',
+        'foto':'angela-caroll-chief-editor.jpg'
     },
     {
-        'name':'Walter Gordon',
-        'role':'Office Manager',
-        'image':'walter-gordon-office-manager.jpg'
+        'nome':'Walter Gordon',
+        'ruolo':'Office Manager',
+        'foto':'walter-gordon-office-manager.jpg'
     },
     {
-        'name':'Angela Lopez',
-        'role':'Social Media Manager',
-        'image':'angela-lopez-social-media-manager.jpg'
+        'nome':'Angela Lopez',
+        'ruolo':'Social Media Manager',
+        'foto':'angela-lopez-social-media-manager.jpg'
     },
     {
-        'name':'Scott Estrada',
-        'role':'Developer',
-        'image':'scott-estrada-developer.jpg'
+        'nome':'Scott Estrada',
+        'ruolo':'Developer',
+        'foto':'scott-estrada-developer.jpg'
     },
     {
-        'name':'Barbara Ramos',
-        'role':'Graphic Designer',
-        'image':'barbara-ramos-graphic-designer.jpg'
+        'nome':'Barbara Ramos',
+        'ruolo':'Graphic Designer',
+        'foto':'barbara-ramos-graphic-designer.jpg'
     },
 ]
+
+for(let i in ourTeam){
+    console.log(`Membro n${parseInt(i)+1}`)
+    let membro = ourTeam[i]
+    for (let j in membro){
+        console.log(membro[j])
+    }
+}
