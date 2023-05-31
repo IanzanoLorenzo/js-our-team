2605377e6060480126742fc3c9e3cc28d3b17b42
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-05-31 19:50:19 +0200

    bonus 1 done

---
+++ bonus-1/script/script.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const messages = document.getElementById('info')
+
+//array contenente i dati del team
+let ourTeam = [
+    {
+        'nome':'Wayne Barnett',
+        'ruolo':'Founder & CEO',
+        'foto':'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        'nome':'Angela Caroll',
+        'ruolo':'Chief Editor',
+        'foto':'angela-caroll-chief-editor.jpg'
+    },
+    {
+        'nome':'Walter Gordon',
+        'ruolo':'Office Manager',
+        'foto':'walter-gordon-office-manager.jpg'
+    },
+    {
+        'nome':'Angela Lopez',
+        'ruolo':'Social Media Manager',
+        'foto':'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        'nome':'Scott Estrada',
+        'ruolo':'Developer',
+        'foto':'scott-estrada-developer.jpg'
+    },
+    {
+        'nome':'Barbara Ramos',
+        'ruolo':'Graphic Designer',
+        'foto':'barbara-ramos-graphic-designer.jpg'
+    },
+]
+
+//ciclo che mostra i dati in console
+for(let i in ourTeam){
+    const membroDiv = document.createElement('div')
+    membroDiv.style.textTransform = 'capitalize'
+    membroDiv.innerHTML = `<h2>Membro numero ${parseInt(i)+1}</h2>`
+    let membro = ourTeam[i]
+    // for (let j in membro){
+    //    membroDiv.innerHTML += `<p>${j}:${membro[j]}</p>`
+    // }
+    membroDiv.innerHTML += `
+    <img src="./img/${membro.foto}">
+    <p>Nome: ${membro.nome} </p>
+    <p>Ruolo: ${membro.ruolo} </p>
+    `
+    messages.append(membroDiv)
+}
